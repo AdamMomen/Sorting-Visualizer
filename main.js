b7c0b4c29d5cb31891d77de0afd49cb5651fa448
AdamMomen <adammuman81@gmail.com>
2020-03-13 16:37:21 +0100

    (fix) fixed synax error & removed pic & removed stop button

--- main.js
+++ main.js
@@ -1,71 +1,4 @@
 // THIS IS THE MAIN JS CODE.
-//===============================================
-// HELPER FUNCTIONS
-function each(coll, f) {
-    if (Array.isArray(coll)) {
-      for (var i = 0; i < coll.length; i++) {
-        f(coll[i], i);
-      }
-    } else {
-      for (var key in coll) {
-        f(coll[key], key);
-      }
-    }
-  }
-  
-  function filter(array, predicate) {
-    var acc = [];
-    each(array, function(element, i) {
-      if (predicate(element, i)) {
-        acc.push(element);
-      }
-    });
-    return acc;
-  }
-  
-  function map(array, func) {
-    var acc = [];
-    each(array, function(element, i) {
-      acc.push(func(element, i));
-    });
-    return acc;
-  }
-  
-  function reduce(array, f, acc) {
-    if (acc === undefined) {
-      acc = array[0];
-      array = array.slice(1);
-    }
-    each(array, function(element, i) {
-      acc = f(acc, element, i);
-    });
-    return acc;
-  }
-
-//*****************************************THIS CODE IS AUXILIARY TO SHOW THE SORTING IN SIMPLER FORM.**************************************
-var RandGenArr = [];
-var arrayLength = 10;
-var maxRandValue = 5;
-
-
-function getRandomInt() {
-  return Math.floor(Math.random() * Math.floor(maxRandValue));
-}
- function move (array,from, to) {
-      var temp =  array.splice(to, 1, array[from]);
-      array.splice(from, 1, temp[0]);
- }
-
-function sortArray(array) {
-  for (var i = 0; i < array.length; i++) {  
-   for(var j = i; j < array.length ; j++) {
-    if (array[i] > array[j] ) {
-       move(array, i, j);
-      }
-    }
-  }
-return array;
-}
 //====================================== MAIN PROJECT CORE ALGROTHIMS ===============================================================
 //Variable block generation
 var mySound = new sound("bounce.mp3");
@@ -75,55 +8,54 @@ const container = document.querySelector(".data-container");
 var sortingOperation = false;
 var exitLoop = false;
 
-//on click on the input checkbox input show the algorithm title on body. 
-$('input').click(function () {
-   var text = $("input:checked").val();
-   $('.header').text(text);
-
-})
+//on click on the input checkbox input show the algorithm title on body.
+$("input").click(function() {
+  var text = $("input:checked").val();
+  $(".header").text(text);
+});
 //on click on Random Block Generate , call generateBlocks
-$('#randblkGenBtn').click(function () {
-if (sortingOperation) {
-  return alert ('SORTING operation is RUNNING PLEASE Wait')
+$("#randblkGenBtn").click(function() {
+  if (sortingOperation) {
+    return alert("SORTING operation is RUNNING PLEASE Wait");
   }
- generateBlocks();
+  generateBlocks();
 });
 //on click sort start sorting.
-$('#sort').click(function () {
- if ($("input:checked").val() === undefined){
-  return alert('Please select sorting algorithm');
- }
+$("#sort").click(function() {
+  if ($("input:checked").val() === undefined) {
+    return alert("Please select sorting algorithm");
+  }
   if (sortingOperation) {
-    return alert ('SORTING OPERATION IS RUNNING PLEASE WAIT')
-    }
+    return alert("SORTING OPERATION IS RUNNING PLEASE WAIT");
+  }
   mergeSort();
   bubbleSort();
   selectionSort();
 });
 //on click on slider show the number of blocks
-$('#Range').click(function () {
-   if (sortingOperation) {
-    return alert('Please wait until the sorting is done');
+$("#Range").click(function() {
+  if (sortingOperation) {
+    return alert("Please wait until the sorting is done");
   }
-    numberOfBlocks = parseInt($("#Range").val());
-    generateBlocks();
-    $('#valOfSlider').text(numberOfBlocks + ' Blocks');
-})
+  numberOfBlocks = parseInt($("#Range").val());
+  generateBlocks();
+  $("#valOfSlider").text(numberOfBlocks + " Blocks");
+});
 //on click on slider show animation delay.
-$('#myTime').click(function () {
-   if (sortingOperation) {
-    return alert('Please wait until the sorting is done');
+$("#myTime").click(function() {
+  if (sortingOperation) {
+    return alert("Please wait until the sorting is done");
   }
-    delayTime = parseInt($("#myTime").val());
-    $('#timeOfSlider').text(delayTime + ' ms ');
+  delayTime = parseInt($("#myTime").val());
+  $("#timeOfSlider").text(delayTime + " ms ");
 });
 //on stop click stop the sorting
-$('#stop').click(function () {
-   if (sortingOperation === false) {
-    return alert('The sorting has already stopped');
+$("#stop").click(function() {
+  if (sortingOperation === false) {
+    return alert("The sorting has already stopped");
   }
-    exitLoop = true;
-    console.log(exitLoop);
+  exitLoop = true;
+  console.log(exitLoop);
 });
 //This function is for sound effect
 function sound(src) {
@@ -133,25 +65,25 @@ function sound(src) {
   this.sound.setAttribute("controls", "none");
   this.sound.style.display = "none";
   document.body.appendChild(this.sound);
-  this.play = function(){
+  this.play = function() {
     this.sound.play();
-  }
-  this.stop = function(){
+  };
+  this.stop = function() {
     this.sound.pause();
-  }
+  };
 }
- //============================>RANDOM BLOCK GENERATOR<==========================//
+//============================>RANDOM BLOCK GENERATOR<==========================//
 function generateBlocks(num = numberOfBlocks) {
-$(container).html('');
+  $(container).html("");
   if (typeof num !== "number") {
     alert("First argument must be a typeof Number");
     return;
   }
   for (let i = 0; i < num; i += 1) {
     const value = Math.floor(Math.random() * 100);
-    //create  cnost of div 
+    //create  cnost of div
     const block = document.createElement("div");
-    //add class named block 
+    //add class named block
     block.classList.add("block");
     //set the height of the block to a random generated value scaled by 3.
     block.style.height = `${value * 3}px`;
@@ -163,10 +95,9 @@ $(container).html('');
     blockLabel.innerHTML = value;
 
     block.appendChild(blockLabel);
-    $(container).append(block)
+    $(container).append(block);
   }
 }
-
 
 //** each page refresh will generate a new random block
 generateBlocks();
@@ -191,171 +122,175 @@ function swap(el1, el2) {
     });
   });
 }
- //===============================>Bubble Sorted Funvtion<===========================//
+//===============================>Bubble Sorted Funvtion<===========================//
 //bubble function that takes the delay time value in ms;
 async function bubbleSort(delay = delayTime) {
-  if ($("input:checked").val() === 'Bubble Sort') {
+  if ($("input:checked").val() === "Bubble Sort") {
     sortingOperation = true;
-    if (  typeof delay !== "number" ) {
-    alert("sort: First argument must be a typeof Number");
-    return;
-  }
+    if (typeof delay !== "number") {
+      alert("sort: First argument must be a typeof Number");
+      return;
+    }
 
-  let blocks = document.querySelectorAll(".block");
-  for (let i = 0; i < blocks.length - 1; i++) {
-    for (let j = 0; j < blocks.length - i - 1; j++) {
+    let blocks = document.querySelectorAll(".block");
+    for (let i = 0; i < blocks.length - 1; i++) {
+      for (let j = 0; j < blocks.length - i - 1; j++) {
         if (exitLoop === true) {
-            break;
-          } else {
-      blocks[j].style.backgroundColor = "#FF4949";
-      blocks[j + 1].style.backgroundColor = "#FF4949";
-      //making delay time , asyncrounsly form the main excution time.
-      await new Promise(resolve =>
-        setTimeout(() => {
-          resolve();
-        }, delay)
-      );
-      //takes the values of index and the following index from blocks div
-      const value1 = Number(blocks[j].childNodes[0].innerHTML);
-      const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+          break;
+        } else {
+          blocks[j].style.backgroundColor = "#FF4949";
+          blocks[j + 1].style.backgroundColor = "#FF4949";
+          //making delay time , asyncrounsly form the main excution time.
+          await new Promise(resolve =>
+            setTimeout(() => {
+              resolve();
+            }, delay)
+          );
+          //takes the values of index and the following index from blocks div
+          const value1 = Number(blocks[j].childNodes[0].innerHTML);
+          const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
 
-      if (value1 > value2) {
-        await swap(blocks[j], blocks[j + 1]);  //calling the swap function and waiting until the transition ends
-        blocks = document.querySelectorAll(".block");
+          if (value1 > value2) {
+            await swap(blocks[j], blocks[j + 1]); //calling the swap function and waiting until the transition ends
+            blocks = document.querySelectorAll(".block");
+          }
+          mySound.play();
+          //set element current index and following to different color style.
+          blocks[j].style.backgroundColor = "#58B7FF";
+          blocks[j + 1].style.backgroundColor = "#58B7FF";
+
+          //set the last element color to green.
+          blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
+          //setting the operation flage to false, ending of sorting .
+        }
+        sortingOperation = false;
+        exitLoop = false;
       }
-        mySound.play();
-      //set element current index and following to different color style.
-      blocks[j].style.backgroundColor = "#58B7FF";
-      blocks[j + 1].style.backgroundColor = "#58B7FF";
-    
-    //set the last element color to green. 
-    blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
-      
-    
-  //setting the operation flage to false, ending of sorting .
- }
-  sortingOperation = false;
-  exitLoop = false;
+    }
+  }
 }
 //==============================>MERGE SORTER FUNCTION<=============================//
 async function mergeSort(delay = delayTime) {
-  if ($("input:checked").val() === 'Merge Sort') {
+  if ($("input:checked").val() === "Merge Sort") {
     sortingOperation = true;
-  if (typeof delay !== "number") {
-    alert("sort: First argument must be a typeof Number");
-    return;
-  }
-  let blocks = document.querySelectorAll(".block");
-  const blocksMiddleIdx = Math.floor((blocks.length - 1) / 2);
-  //THIS PART FOR THE FIRST HALF OF THE BLOCK.
-  for (let i = 0; i < blocksMiddleIdx ; i++) {
-    for (let j = 0; j < blocksMiddleIdx - i ; j++) {
-      //this will break the function if the stop button cliked.
-      if (exitLoop === true) {
-      break;
-      } else {
-    blocks[j].style.backgroundColor = "#FF4949";
-    blocks[j + 1].style.backgroundColor = "#FF4949";
-     await new Promise(resolve =>
-        setTimeout(() => {
-          resolve();
-        }, delay)
-      );
-      //takes the values of index and the followig index from blocks div
-      const value1 = Number(blocks[j].childNodes[0].innerHTML);
-      const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
-
-      if (value1 > value2) {
-        await swap(blocks[j], blocks[j + 1]);  //calling the swap function and waiting until the transition ends
-        blocks = document.querySelectorAll(".block");
-      }
-      //play the sound effect.
-      mySound.play();
-      //set element current index and following to different color style.
-      blocks[j].style.backgroundColor = "#58B7FF";
-      blocks[j + 1].style.backgroundColor = "#58B7FF";
-    
-    //set the last element color to green. 
-    blocks[blocksMiddleIdx - i ].style.backgroundColor = "#13CE66";
-
-    //THIS IS FOR THE NEXT HALF OF THE BLOCKS.
-    for (let i = blocksMiddleIdx + 1; i < blocks.length - 1 ; i++) {
-      for (let j = blocksMiddleIdx + 1; j <blocks.length - i - 1 ; j++) {
-
-      blocks[j].style.backgroundColor = "#FF4949";
-      blocks[j + 1].style.backgroundColor = "#FF4949";
-      await new Promise(resolve =>
-        setTimeout(() => {
-          resolve();
-        }, delay)
-      );
-
-      //takes the values of index and the followig index from blocks div
-      const value1 = Number(blocks[j].childNodes[0].innerHTML);
-      const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
-
-       if (value1 > value2) {
-        await swap(blocks[j], blocks[j + 1]);  //calling the swap function and waiting until the transition ends
-        blocks = document.querySelectorAll(".block");
-      }
-       //set element current index and following to different color style.
-      blocks[j].style.backgroundColor = "#58B7FF";
-      blocks[j + 1].style.backgroundColor = "#58B7FF";
+    if (typeof delay !== "number") {
+      alert("sort: First argument must be a typeof Number");
+      return;
     }
-    //set the last element color to green. 
-    blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
-    //setting the operation flage to false, ending of sorting .
+    let blocks = document.querySelectorAll(".block");
+    const blocksMiddleIdx = Math.floor((blocks.length - 1) / 2);
+    //THIS PART FOR THE FIRST HALF OF THE BLOCK.
+    for (let i = 0; i < blocksMiddleIdx; i++) {
+      for (let j = 0; j < blocksMiddleIdx - i; j++) {
+        //this will break the function if the stop button cliked.
+        if (exitLoop === true) {
+          break;
+        } else {
+          blocks[j].style.backgroundColor = "#FF4949";
+          blocks[j + 1].style.backgroundColor = "#FF4949";
+          await new Promise(resolve =>
+            setTimeout(() => {
+              resolve();
+            }, delay)
+          );
+          //takes the values of index and the followig index from blocks div
+          const value1 = Number(blocks[j].childNodes[0].innerHTML);
+          const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+
+          if (value1 > value2) {
+            await swap(blocks[j], blocks[j + 1]); //calling the swap function and waiting until the transition ends
+            blocks = document.querySelectorAll(".block");
+          }
+          //play the sound effect.
+          mySound.play();
+          //set element current index and following to different color style.
+          blocks[j].style.backgroundColor = "#58B7FF";
+          blocks[j + 1].style.backgroundColor = "#58B7FF";
+
+          //set the last element color to green.
+          blocks[blocksMiddleIdx - i].style.backgroundColor = "#13CE66";
+
+          //THIS IS FOR THE NEXT HALF OF THE BLOCKS.
+          for (let i = blocksMiddleIdx + 1; i < blocks.length - 1; i++) {
+            for (let j = blocksMiddleIdx + 1; j < blocks.length - i - 1; j++) {
+              blocks[j].style.backgroundColor = "#FF4949";
+              blocks[j + 1].style.backgroundColor = "#FF4949";
+              await new Promise(resolve =>
+                setTimeout(() => {
+                  resolve();
+                }, delay)
+              );
+
+              //takes the values of index and the followig index from blocks div
+              const value1 = Number(blocks[j].childNodes[0].innerHTML);
+              const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+
+              if (value1 > value2) {
+                await swap(blocks[j], blocks[j + 1]); //calling the swap function and waiting until the transition ends
+                blocks = document.querySelectorAll(".block");
+              }
+              //set element current index and following to different color style.
+              blocks[j].style.backgroundColor = "#58B7FF";
+              blocks[j + 1].style.backgroundColor = "#58B7FF";
+            }
+            //set the last element color to green.
+            blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
+            //setting the operation flage to false, ending of sorting .
+          }
         }
+        //reseting the operation and exit state flags.
+        sortingOperation = false;
+        exitLoop = false;
       }
-      //reseting the operation and exit state flags.
-  sortingOperation = false;
-  exitLoop = false;
+    }
   }
-} 
+}
 //=============================================SELECTION SORT ALGORITMH==========================================
 async function selectionSort(delay = delayTime) {
-  if ($("input:checked").val() === 'Selection Sort') {
+  if ($("input:checked").val() === "Selection Sort") {
     sortingOperation = true;
-    if (  typeof delay !== "number" ) {
-    alert("sort: First argument must be a typeof Number");
-    return;
-  }
+    if (typeof delay !== "number") {
+      alert("sort: First argument must be a typeof Number");
+      return;
+    }
 
-  let blocks = document.querySelectorAll(".block");
-  for (let i = 0; i < blocks.length - 1; i++) {
-    for (let j = 0; j < blocks.length - i - 1; j++) {
+    let blocks = document.querySelectorAll(".block");
+    for (let i = 0; i < blocks.length - 1; i++) {
+      for (let j = 0; j < blocks.length - i - 1; j++) {
         if (exitLoop === true) {
-            break;
-          } else {
-      blocks[j].style.backgroundColor = "#FF4949";
-      blocks[j + 1].style.backgroundColor = "#FF4949";
-      //making delay time , asyncrounsly form the main excution time.
-      await new Promise(resolve =>
-        setTimeout(() => {
-          resolve();
-        }, delay)
-      );
-      //takes the values of index and the following index from blocks div
-      const value1 = Number(blocks[j].childNodes[0].innerHTML);
-      const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+          break;
+        } else {
+          blocks[j].style.backgroundColor = "#FF4949";
+          blocks[j + 1].style.backgroundColor = "#FF4949";
+          //making delay time , asyncrounsly form the main excution time.
+          await new Promise(resolve =>
+            setTimeout(() => {
+              resolve();
+            }, delay)
+          );
+          //takes the values of index and the following index from blocks div
+          const value1 = Number(blocks[j].childNodes[0].innerHTML);
+          const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
 
-      if (value1 < value2) {
-        await swap(blocks[j], blocks[j + 1]);  //calling the swap function and waiting until the transition ends
-        blocks = document.querySelectorAll(".block");
+          if (value1 < value2) {
+            await swap(blocks[j], blocks[j + 1]); //calling the swap function and waiting until the transition ends
+            blocks = document.querySelectorAll(".block");
+          }
+
+          //play the sound effect
+          mySound.play();
+          //set element current index and following to different color style.
+          blocks[j].style.backgroundColor = "#58B7FF";
+          blocks[j + 1].style.backgroundColor = "#58B7FF";
+
+          //set the last element color to green.
+          blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
+
+          //setting the operation flage to false, ending of sorting .
+        }
+        sortingOperation = false;
+        exitLoop = false;
       }
-
-      //play the sound effect
-      mySound.play();
-      //set element current index and following to different color style.
-      blocks[j].style.backgroundColor = "#58B7FF";
-      blocks[j + 1].style.backgroundColor = "#58B7FF";
-    
-    //set the last element color to green. 
-    blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
-      
-    
-  //setting the operation flage to false, ending of sorting .
- }
-  sortingOperation = false;
-  exitLoop = false;
+    }
+  }
 }
